a8da79f455689e97beee362c52338a60525fbc34
inf2147483647 <2043206762@qq.com>
2026-07-17 08:52:40 +0800

    release: bump version to 1.0.2.0 and update changelog
    1. update plugin version from 1.0.1.3 to 1.0.2.0 in manifest and about page
    2. mark periodic countdown feature as completed in README
    3. add new planned tasks in README

--- Views/Settings/AboutPage.cs
+++ Views/Settings/AboutPage.cs
@@ -317,7 +317,7 @@ public class AboutPage : SettingsPageBase
 
         var versionText = new TextBlock
         {
-            Text = "版本：1.0.1.3",
+            Text = "版本：1.0.2.0",
             FontSize = 14,
             Foreground = ThemeHelper.GetTextBrush()
         };
